86fdc5f3e3cd69d8406f6fe27be89b1439b45bc2
yamaoka <yamaoka>
2008-06-18 23:45:30 +0000

    (w3m-submit-form): Fix previous commit.

--- w3m-util.el
+++ w3m-util.el
@@ -1019,9 +1019,10 @@ the region active."
 	 (setq zmacs-region-stays t))))
 
 (defmacro w3m-deactivate-region ()
-  "Deactivate the region."
-  (if (featurep 'xemacs)
-      '(zmacs-deactivate-region)
+  "Deactivate the region.
+This macro does nothing in XEmacs, because the region is always
+deactivated after evaluating the current command."
+  (unless (featurep 'xemacs)
     '(deactivate-mark)))
 
 (defmacro w3m-region-active-p ()
